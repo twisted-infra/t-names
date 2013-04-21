112c8019516d471af68c622e18980bd690119b6c
Tom Prince <tom.prince@ualberta.net>
2013-04-21 13:49:51 -0600

    Update to use 'task_' prefix.

--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,7 @@ Support for DNS service installation and management.
 
 from fabric.api import run, settings
 
-from braid import authbind, requiresRoot, git  #, cron
+from braid import authbind, git  #, cron
 from braid.twisted import service
 
 # TODO: Move these somewhere else and make them easily extendable
@@ -12,14 +12,12 @@ from braid.config import test, prod, environment
 
 
 class TwistedNames(service.Service):
-    @service.task
-    @requiresRoot
-    def install(self):
+    def task_install(self):
         """
         Install t-names, a Twisted Names based DNS server.
         """
         # Bootstrap a new service environment
-        self.bootstrap()
+        self.task_bootstrap()
 
         # Setup authbind
         authbind.install()
@@ -30,12 +28,11 @@ class TwistedNames(service.Service):
 
         with settings(user=self.serviceUser):
             run('ln -nsf {}/start {}/start'.format(self.srcDir, self.binDir))
-            self.update()
+            self.task_update()
             # FIXME: Uncomment once cron.py is commited to braid
             #cron.install(env.user, 'Names/crontab')
 
-    @service.task
-    def update(self):
+    def task_update(self):
         with settings(user=self.serviceUser):
             # TODO: This is a temp location for testing
             git.branch('https://github.com/twisted-infra/t-names', self.srcDir)
